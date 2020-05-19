66ece6d8de3f2a9a860401aeea7f04a3f1581b0b
stuart-bass-bjss <59572183+stuart-bass-bjss@users.noreply.github.com>
2020-05-19 08:56:47 +0100

    azure mostly working

--- launch-or-update-azure.sh
+++ launch-or-update-azure.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 namespace="buyingcatalogue-master"
-basePath="$namespace.dev.buyingcatalogue.digital.nhs.uk"
+basePath="$namespace-dev.buyingcatalogue.digital.nhs.uk"
 baseUrl="https://$basePath"
 baseIdentityUrl="$baseUrl/identity"
 
-helm upgrade bc gpitfuturesdevacr/buyingcatalogue -n $namespace -i -f environments/azure.yaml --debug \
+#helm upgrade bc gpitfuturesdevacr/buyingcatalogue -n $namespace -i -f environments/azure.yaml --debug \
+helm upgrade bc src/buyingcatalogue -n $namespace -i -f environments/azure.yaml \
   --set dbPassword=DisruptTheMarket1! \
   --set clientSecret=SampleClientSecret \
   --set appBaseUrl=$baseUrl \
@@ -16,10 +17,16 @@ helm upgrade bc gpitfuturesdevacr/buyingcatalogue -n $namespace -i -f environmen
   --set isapi.clients[0].postLogoutRedirectUrls[1]=$baseUrl/admin/signout-callback-oidc \
   --set isapi.clients[0].postLogoutRedirectUrls[2]=$baseUrl/order/signout-callback-oidc \
   --set isapi.ingress.hosts[0].host=$basePath \
+  --set oapi.hostAliases[0].hostnames[0]=$basePath \
+  --set ordapi.hostAliases[0].hostnames[0]=$basePath \
   --set email.ingress.hosts[0].host=$basePath \
   --set mp.ingress.hosts[0].host=$basePath \
   --set pb.ingress.hosts[0].host=$basePath \
   --set pb.baseUri=$baseUrl \
+  --set pb.hostAliases[0].hostnames[0]=$basePath \
   --set admin.ingress.hosts[0].host=$basePath \
+  --set admin.hostAliases[0].hostnames[0]=$basePath \
   --set of.ingress.hosts[0].host=$basePath \
+  --set of.hostAliases[0].hostnames[0]=$basePath \
   --set redis-commander.ingress.hosts[0].host=$basePath \
+  
