3d837f94c6c5014f3bc54825b19fda77a193a2e9
stuart-bass-bjss <59572183+stuart-bass-bjss@users.noreply.github.com>
2020-05-26 14:43:37 +0100

    added in changes for certificate changes

--- launch-or-update-local.sh
+++ launch-or-update-local.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+context=`kubectl config current-context`
+if [[ "$context" != "docker-desktop" ]]; then 
+  >&2 echo "Not Local Context - $context"
+  exit 1
+fi
+
 if [[ "$OSTYPE" == "darwin"* ]]; then 
   helm upgrade bc src/buyingcatalogue -n buyingcatalogue -i -f environments/local-docker.yaml -f environments/local-docker-mac.yaml -f local-overrides.yaml $@
 else
