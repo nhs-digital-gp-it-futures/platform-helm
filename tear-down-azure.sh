99c59645f428928cd285f29527199dbb2d7812e7
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-19 16:53:49 +0100

    Commit to trigger job for PR(#43)
    added support for deleting blob store

--- tear-down-azure.sh
+++ tear-down-azure.sh
@@ -1,6 +1,73 @@
 #!/bin/bash
 
-commitMessage=$1
+function displayHelp {
+  printf "usage: ./tear-down-azure.sh [OPTIONS]
+          -h, --help
+            Display help
+          -m, --commit-message <commit-message>
+            Message of the latest commit on this branch
+          -d, --db-server <database server>
+            [REQUIRED] SQL database server to deploy to
+          -u, --db-admin-user <user name>
+            [REQUIRED] SQL Admin User Name
+          -p, --db-admin-pass <password>
+            [REQUIRED] SQL Admin Password
+          -a, --azure-storage-connection-string <connection string>
+            [REQUIRED] Azure Storage Connection String
+          "
+  exit
+}
+
+# Option strings
+SHORT="hm:d:u:p:a:"
+LONG="help,commit-message:,db-server:,db-admin-user:,db-admin-pass:,azure-storage-connection-string:"
+
+# read the options
+OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+if [ $? != 0 ] ; then echo "Failed to parse options... exiting." >&2 ; exit 1 ; fi
+eval set -- "$OPTS"
+
+
+# extract options and their arguments into variables.
+while true ; do
+  case "$1" in
+    -h | --help )
+      displayHelp
+      shift
+      ;;
+    -m | --commit-message )
+      commitMessage="$2"
+      shift 2
+      ;;
+    -d | --db-server )
+      dbServer="$2"
+      shift 2
+      ;;
+    -u | --db-admin-user )
+      saUserName="$2"
+      shift 2
+      ;;
+    -p | --db-admin-pass )
+      saPassword="$2"
+      shift 2
+      ;;
+    -a | --azure-storage-connection-string )
+      azureStorageConnectionString="$2"
+      shift 2
+      ;;
+    -- )
+      shift
+      break
+      ;;
+    *)
+      echo "Internal error!"
+      exit 1
+      ;;
+  esac
+done
+
+
+
 containsReferenceToPR='^(.*)([#][0-9]{1,3})[^0-9](.*)$'
 
 
@@ -17,10 +84,12 @@ echo $branchName
 branchNamespace=`echo $branchName | sed 's/feature[[:punct:]]/bc-/g'`
 prNamespace="bc-merge-$prNumber"
 
-echo "going to delete ns $branchNamespace"
-echo "going to delete ns $prNamespace"
+echo "helm delete bc -n $branchNamespace"
+echo "kubectl delete ns $prNamespace"
 
+#test az storage works
+az storage blob list -c $branchName --connection-string "$azureStorageConnectionString"
 
-# helm delete bc -n $namespace
+echo `az storage blob delete -c $branchNamespace --connection-string "$azureStorageConnectionString"`
+echo `az storage blob delete -c $prNamespace --connection-string "$azureStorageConnectionString"`
 
-# kubectl delete ns $namespace
