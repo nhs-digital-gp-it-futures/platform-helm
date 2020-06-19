50aa7f7b9b6107a05c3386a02b3877763e14e0f6
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-19 17:10:41 +0100

    Commit for fake PR merge (#43)
    Use the correct syntax for az storage

--- tear-down-azure.sh
+++ tear-down-azure.sh
@@ -73,14 +73,13 @@ containsReferenceToPR='^(.*)([#][0-9]{1,3})[^0-9](.*)$'
 
 if [[ $commitMessage =~ $containsReferenceToPR ]]; then
     prNumber=$(echo ${BASH_REMATCH[2]} | tr -d '#') # grab the matched group
-    echo $prNumber
 else
    echo "Couldn't extract PR number from the commit message, exiting."
    exit 1
 fi
 
 branchName=$(curl https://api.github.com/repos/nhs-digital-gp-it-futures/platform-helm/pulls/$prNumber | jq --raw-output '.head.ref')
-echo $branchName
+
 branchNamespace=`echo $branchName | sed 's/feature[[:punct:]]/bc-/g'`
 prNamespace="bc-merge-$prNumber"
 
@@ -88,8 +87,8 @@ echo "helm delete bc -n $branchNamespace"
 echo "kubectl delete ns $prNamespace"
 
 #test az storage works
-az storage blob list -c $branchName --connection-string "$azureStorageConnectionString"
+az storage container list --connection-string "$azureStorageConnectionString"
 
-echo `az storage blob delete -c $branchNamespace --connection-string "$azureStorageConnectionString"`
-echo `az storage blob delete -c $prNamespace --connection-string "$azureStorageConnectionString"`
+#az storage container delete --name $branchNamespace --connection-string "$azureStorageConnectionString"
+#az storage container delete --name $prNamespace --connection-string "$azureStorageConnectionString"
 
