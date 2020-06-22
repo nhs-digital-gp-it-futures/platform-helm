0e201dd8b9533f919107d067d27970880f1c9b78
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-22 14:43:25 +0100

    Refactor tear-down-azure.sh
    - now accepts --namespace arg, which will skip trying to calculate the ns from commit message

--- tear-down-azure.sh
+++ tear-down-azure.sh
@@ -4,6 +4,8 @@ function displayHelp {
   printf "usage: ./tear-down-azure.sh [OPTIONS]
           -h, --help
             Display help
+          -n, --namespace <namespace>
+            Name of the namespace to delete
           -m, --commit-message <commit-message>
             Message of the latest commit on this branch
           -d, --db-server <database server>
@@ -19,14 +21,13 @@ function displayHelp {
 }
 
 # Option strings
-SHORT="hm:d:u:p:a:"
-LONG="help,commit-message:,db-server:,db-admin-user:,db-admin-pass:,azure-storage-connection-string:"
+SHORT="hn:m:d:u:p:a:"
+LONG="help,namespace:,commit-message:,db-server:,db-admin-user:,db-admin-pass:,azure-storage-connection-string:"
 
 # read the options
 OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
 if [ $? != 0 ] ; then echo "Failed to parse options... exiting." >&2 ; exit 1 ; fi
 eval set -- "$OPTS"
-
 
 # extract options and their arguments into variables.
 while true ; do
@@ -34,6 +35,10 @@ while true ; do
     -h | --help )
       displayHelp
       shift
+      ;;
+    -n | --namespace )
+      namespace="$2"
+      shift 2
       ;;
     -m | --commit-message )
       commitMessage="$2"
@@ -66,36 +71,76 @@ while true ; do
   esac
 done
 
+function calculatePrNumber {
+    containsReferenceToPR='^(.*)([#][0-9]{1,3})[^0-9]?(.*)$'
 
-containsReferenceToPR='^(.*)([#][0-9]{1,3})[^0-9]?(.*)$'
+    if [[ $commitMessage =~ $containsReferenceToPR ]]; then
+        prNumber=$(echo ${BASH_REMATCH[2]} | tr -d '#') # grab the matched group
+    else
+        echo "Couldn't extract PR number from the commit message, exiting."
+        exit 0
+    fi
+}
 
-if [[ $commitMessage =~ $containsReferenceToPR ]]; then
-    prNumber=$(echo ${BASH_REMATCH[2]} | tr -d '#') # grab the matched group
-else
-   echo "Couldn't extract PR number from the commit message, exiting."
-   exit 0
+function calculateBranchName {
+    branchName=$(curl https://api.github.com/repos/nhs-digital-gp-it-futures/platform-helm/pulls/$prNumber | jq --raw-output '.head.ref' )
+}
+
+function calculateNamespaceNames {
+    if [ ! $1 ]; then
+        branchNamespace=`echo $branchName | sed 's/feature[[:punct:]]/bc-/g'`
+        prNamespace="bc-merge-$prNumber"
+        return 
+    fi
+    
+    branchNamespace="$1"
+}
+
+function deleteKuberetesResources {
+    kubectl delete ns $branchNamespace $prNamespace || true
+}
+
+function deleteBlobStoreContainers {
+    az storage container delete --name "$branchNamespace-documents" --connection-string "$azureStorageConnectionString" || true
+    az storage container delete --name "$prNamespace-documents" --connection-string "$azureStorageConnectionString" || true
+}
+
+function deleteDatabases {
+    # modify IFS to allow spaces in array elements
+    IFS=""
+    services=("bapi"  "isapi"  "ordapi")
+    deleteQueries=()
+
+    for service in ${services[*]}; do
+        deleteQueries+=("DROP DATABASE [bc-$branchNamespace-$service];")
+        deleteQueries+=("DROP DATABASE [bc-$prNamespace-$service];")
+    done
+
+    for query in ${deleteQueries[*]}; do
+        sqlcmd -S $dbServer -U "$saUserName" -P "$saPassword" -d master -Q $query || true
+    done
+}
+
+function deleteAllResources {
+    deleteKuberetesResources
+    deleteBlobStoreContainers
+    deleteDatabases
+}
+
+if [ -z "$commitMessage" ]; then
+    echo "missing commit message argument, relying on provided namespace name..."
+    calculateNamespaceNames "$namespace"
+    deleteAllResources
+    exit
 fi
 
-branchName=$(curl https://api.github.com/repos/nhs-digital-gp-it-futures/platform-helm/pulls/$prNumber | jq --raw-output '.head.ref')
+if [ -z "$commitMessage" ] && [ -z "$namespace" ]; then
+    echo "missing commit message and namespace name, cannot calculate namespace name, exiting..."
+    exit
+fi
 
-branchNamespace=`echo $branchName | sed 's/feature[[:punct:]]/bc-/g'`
-prNamespace="bc-merge-$prNumber"
+calculatePrNumber
+calculateBranchName
+calculateNamespaceNames
 
-kubectl delete ns $branchNamespace $prNamespace || true 
-
-az storage container delete --name "$branchNamespace-documents" --connection-string "$azureStorageConnectionString" || true 
-az storage container delete --name "$prNamespace-documents" --connection-string "$azureStorageConnectionString" || true 
-
-# modify IFS to allow spaces in array elements
-IFS=""
-services=("bapi"  "isapi"  "ordapi")
-deleteQueries=()
-
-for service in ${services[*]}; do
-     deleteQueries+=("DROP DATABASE [bc-$branchNamespace-$service];")
-     deleteQueries+=("DROP DATABASE [bc-$prNamespace-$service];")
-done
-
-for query in ${deleteQueries[*]}; do
-     sqlcmd -S $dbServer -U "$saUserName" -P "$saPassword" -d master -Q $query || true
-done
+deleteAllResources
