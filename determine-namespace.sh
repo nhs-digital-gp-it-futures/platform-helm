00d2f85594a2b9d9324e942f7a9cb32765e27ac1
Adrian Soomro <37105695+assoomro@users.noreply.github.com>
2020-06-30 10:38:47 +0100

    Feature/7958 push to same feature branch (#57)
    * Deploy to existing namespace on local builds & builds triggered by remote pipelines
    AB#7958
    AB#7966

---
+++ determine-namespace.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+function extractStoryIdFromBranchName {
+    storyIdRegex='^(refs/heads/feature/)([0-9]{4,5})[^0-9]?(.*)$'
+
+    if [[ "$1" =~ $storyIdRegex ]]; then
+      storyId=$(echo ${BASH_REMATCH[2]}) # get the 2nd captured group
+    else
+        >&2 echo "Couldn't extract the story Id from branch name, exiting."
+        exit 1
+    fi
+}
+
+function calculateBranchNameFromBranchOfRemoteTrigger {
+  # Gets name of the variable following given pattern
+  branchNameVariableName=$(compgen -A variable | grep 'RESOURCES_PIPELINE_.*SOURCEBRANCH')
+
+  # returns the value from the variable name captured above
+  echo ${!branchNameVariableName}
+  return 0
+}
+
+function calculateNamespaceFromBranchName {
+  branchName=$1
+
+  extractStoryIdFromBranchName $branchName
+
+  allNamespaces=$(kubectl get ns -o=custom-columns=NAME:.metadata.name)
+
+  for namespace in ${allNamespaces[*]}; do
+    if [[ "$namespace" == *"$storyId"* ]]; then
+      featureNamespace=$namespace
+      break
+    fi
+  done
+
+  if [ -z "$featureNamespace" ]; then
+    unwantedPrefix="refs/heads/"
+    featureNamespace=$(echo "${branchName#${unwantedPrefix}}" | sed 's/[[:punct:]]/-/g')
+  fi
+
+  echo "$featureNamespace"
+  return 0
+}
+
+
+if [ "$BUILD_REASON" = "PullRequest" ]; then
+    namespace=$(echo "bc-$BUILD_SOURCEBRANCHNAME-$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER" | sed 's/[[:punct:]]/-/g')
+elif [ "$BUILD_REASON" = "ResourceTrigger" ]; then
+    branchName=$(calculateBranchNameFromBranchOfRemoteTrigger)
+    namespace=$(calculateNamespaceFromBranchName $branchName)
+else
+    namespace=$(calculateNamespaceFromBranchName $BUILD_SOURCEBRANCH)
+fi
+
+echo "namespace=$namespace"
+echo "##vso[task.setvariable variable=Namespace;isOutput=true]$namespace"
