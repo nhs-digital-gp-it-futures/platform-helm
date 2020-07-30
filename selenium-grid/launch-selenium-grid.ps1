d032e20219db8ec02afb3faab6fbc9347f031d27
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-30 09:35:04 +0100

    Move 'selenium-grid' repo here

---
+++ selenium-grid/launch-selenium-grid.ps1
@@ -0,0 +1,9 @@
+﻿$namespace="selenium-grid"
+
+$response=$(kubectl get ns $namespace)
+if ($response -ne "")
+{
+    kubectl apply -f grid-namespace.yml
+}
+
+helm upgrade sel-grid stable/selenium -i -f values.yaml -n $namespace
