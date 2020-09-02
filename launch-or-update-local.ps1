563cea47e012a033270b57a0f033ae14ee5d9da4
unknown <j.r.alsop.18@gmail.com>
2020-09-02 12:10:35 +0100

    Adding additional parameter to launch scripts

--- launch-or-update-local.ps1
+++ launch-or-update-local.ps1
@@ -1,20 +1,20 @@
-#######################################################
-### Deploy local cluster                            ###
-### usage:                                          ###
-###  ./launch-or-update-local.ps1                   ###
-###                                                 ###
-### without setting repo latest versions            ###
-###  ./launch-or-update-local.ps1 -latest false     ###
-###  ./launch-or-update-local.ps1 -l                ###
-###                                                 ###
-### without downloading updates for set versions    ###
-###  ./launch-or-update-local.ps1 -update false     ###
-###  ./launch-or-update-local.ps1 -u                ###
-###                                                 ###
-### without refreshing at all...                    ###
-###  ./launch-or-update-local.ps1 -r                ###
-###  ./launch-or-update-local.ps1 -useRemote false  ###
-#######################################################
+##########################################################
+### Deploy local cluster                               ###
+### usage:                                             ###
+###  ./launch-or-update-local.ps1                      ###
+###                                                    ###
+### without setting repo latest versions               ###
+###  ./launch-or-update-local.ps1 -latest false        ###
+###  ./launch-or-update-local.ps1 -l                   ###
+###                                                    ###
+### without downloading updates for set versions       ###
+###  ./launch-or-update-local.ps1 -updateCharts false  ###
+###  ./launch-or-update-local.ps1 -u                   ###
+###                                                    ###
+### without refreshing at all...                       ###
+###  ./launch-or-update-local.ps1 -r                   ###
+###  ./launch-or-update-local.ps1 -useRemote false     ###
+##########################################################
 
 param(
         [Parameter()]
@@ -25,22 +25,29 @@ param(
         [ValidateSet('true','false')]
         [string]$useRemote,
         [Parameter()]
-        [switch]$r=$false
+        [switch]$r=$false,
+        [ValidateSet('true','false')]
+        [string]$updateCharts,
+        [Parameter()]
+        [switch]$r=$true
     )
 
 $chart="src/buyingcatalogue"
 
 if (($useRemote -ne "false") -and ($r -eq $false))
 {
-    if (($latest -ne "false") -and ($l -eq $false))
-    {
-        write-host "Getting Latest Chart Versions..."
-        invoke-expression -Command "./update-chart-versions.ps1 -chart $chart -v development"
-    }
-    else
-    {
-        write-host "Getting Master Chart Versions..."
-        invoke-expression -Command "./update-chart-versions.ps1 -chart $chart -v master"
+    if(($updateCharts -ne "false"))
+    {    
+        if (($latest -ne "false") -and ($l -eq $false))
+        {
+            write-host "Getting Latest Chart Versions..."
+            invoke-expression -Command "./update-chart-versions.ps1 -chart $chart -v development"
+        }
+        else
+        {
+            write-host "Getting Master Chart Versions..."
+            invoke-expression -Command "./update-chart-versions.ps1 -chart $chart -v master"
+        }
     }
 
     write-host "`nUpdating Dependencies..."
