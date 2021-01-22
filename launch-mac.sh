7ea98543c12bf2742d9c0c58f189c0d56362a609
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-01-22 13:11:17 +0000

    Update mac scripts

--- launch-mac.sh
+++ launch-mac.sh
@@ -4,17 +4,18 @@ os=$(uname -s)
 
 if [[ "$os" != "Darwin" ]]; then 
   >&2 echo -e "Script is not running on a mac!\n"
-  #exit 1
+  sleep 2
+  exit 1
 fi
 
-#if [ -f "/usr/local/bin/bash" ]; then
-if [ -f "/usr/local/bin/helm" ]; then ##### SWITCHME!!!
+if [ -f "/usr/local/bin/bash" ]; then
+#if [ -f "/usr/local/bin/helm" ]; then ##### SWITCHME!!!
   echo -e "Correct Bash version detected\n"
 else
-  echo "Old Bash version detected: $BASH_VERSION"
+  echo -e "Unsupported Bash version detected: $BASH_VERSION"
   echo -e 'To run Buying Catalogue Scripts on Mac you will need to update Bash to a later version:'
-  echo '-> See https://itnext.io/upgrading-bash-on-macos-7138bd1066ba'
-  echo '-> Once done you will need to restart your terminal session'
+  echo -e '-> See https://itnext.io/upgrading-bash-on-macos-7138bd1066ba'
+  echo -e '-> Once done you will need to restart your terminal session'
   
   sleep 20
   exit 1
@@ -36,16 +37,16 @@ if [[ $optionSelected = "x" ]]; then
   exit 0
 elif [[ $optionSelected = "1" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source ./src/scripts/bash-osx/launch-or-update-local-osx.sh -l false
+  source ./src/scripts/bash-mac/launch-or-update-local-mac.sh -l false
 elif [[ $optionSelected = "2" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source ./src/scripts/bash-osx/launch-or-update-local-osx.sh
+  source ./src/scripts/bash-mac/launch-or-update-local-mac.sh
 elif [[ $optionSelected = "3" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source ./src/scripts/bash-osx/update-chart-versions-osx.sh
+  source ./src/scripts/bash-mac/update-chart-versions-mac.sh
 elif [[ $optionSelected = "4" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source ./src/scripts/bash-osx/update-chart-versions-osx.sh -v development
+  source ./src/scripts/bash-mac/update-chart-versions-mac.sh -v development
 else
   echo "Unrecognised response ($optionSelected) - please try again..."
   sleep 5
