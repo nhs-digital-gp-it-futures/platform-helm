aba09b0fdeb4e25c107561c4bf132b28274ff0ff
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-01-25 10:43:04 +0000

    updated launch script debug

--- launch-mac.sh
+++ launch-mac.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+scriptPath="./src/scripts/bash-mac"
+bashPath="/usr/local/bin/bash"
+#bashPath="/bin/bash"
+
 os=$(uname -s)
 
 if [[ "$os" != "Darwin" ]]; then 
   >&2 echo -e "Script is not running on a mac!\n"
   sleep 2
-  #exit 1
+  exit 1
 fi
 
-if [ -f "/usr/local/bin/bash" ]; then
+if [ -f "$bashPath" ]; then
   echo -e "Correct Bash version detected\n"
 else
   echo -e "Unsupported Bash version detected: $BASH_VERSION"
@@ -16,18 +20,19 @@ else
   echo -e '-> See https://itnext.io/upgrading-bash-on-macos-7138bd1066ba'
   echo -e '-> Once done you will need to restart your terminal session'
   
-  #sleep 10
-  #exit 1
+  sleep 10
+  exit 1
 fi
 
-scriptPath="./src/scripts/bash-mac"
 optionSelected=$1
+
+export optionSelected
 
 if [[ $optionSelected = "debug" ]]; then
   echo -e "\nDEBUG MODE"
   mkdir -p "$scriptPath/logs"
   optionSelected=''
-  ls -R 2>&1 | tee "$scriptPath/logs/folderStructure.txt"
+  ls -R > "$scriptPath/logs/folderStructure.txt"
 fi
 
 if [ -z "$optionSelected" ]; then
@@ -59,27 +64,27 @@ if [[ $optionSelected = "x" ]]; then
   exit 0
 elif [[ $optionSelected = "1" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/launch-or-update-local-mac.sh -l false 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/launch-or-update-local-mac.sh -l false 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "2" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/launch-or-update-local-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/launch-or-update-local-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "3" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/update-chart-versions-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/update-chart-versions-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "4" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/update-chart-versions-mac.sh -v development 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/update-chart-versions-mac.sh -v development 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "5" ]]; then
   kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
   sleep 10
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/start-dashboard-proxy-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/start-dashboard-proxy-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "6" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/start-dashboard-proxy-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/start-dashboard-proxy-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 elif [[ $optionSelected = "7" ]]; then
   echo -e "<---------STARTING SCRIPT---------->\n"
-  source $scriptPath/tear-down-local-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
+  $bashPath $scriptPath/tear-down-local-mac.sh 2>&1 | tee "$scriptPath/logs/$optionSelected-Outputlogs.txt"
 else
   echo "Unrecognised response ($optionSelected) - please try again..."
   sleep 5
