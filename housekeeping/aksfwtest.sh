32540fbfeae0661e601c26ae164a7acf16df404e
ayaz <mohammed.nawaz@mastek.com>
2021-02-08 13:22:03 +0000

    Update aksfwtest.sh

--- housekeeping/aksfwtest.sh
+++ housekeeping/aksfwtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FWStatus=$(az aks show -n gpitfutures-development-aks -g gpitfutures-development-rg-aks --query apiServerAccessProfile.authorizedIpRanges)
-if [${FWStatus} != ""]; echo "AKS FW is Running"
+if [${FWStatus} != ""]; echo "AKS FW is Running" else { echo "AKS FW is off" } 
 
 
 
